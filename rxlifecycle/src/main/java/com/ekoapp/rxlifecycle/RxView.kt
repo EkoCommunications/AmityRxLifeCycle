9b6e9b45f2bd8ee5f60cc6690553212a53a60397
Pan <theptai@ekoapp.com>
2021-03-10 15:33:35 +0700

    lifecycleProviderFromView

--- rxlifecycle/src/main/java/com/ekoapp/rxlifecycle/RxView.kt
+++ rxlifecycle/src/main/java/com/ekoapp/rxlifecycle/RxView.kt
@@ -2,12 +2,41 @@ package com.ekoapp.rxlifecycle
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.LinearLayout
 import com.trello.rxlifecycle3.LifecycleProvider
 import com.trello.rxlifecycle3.LifecycleTransformer
 import com.trello.rxlifecycle3.RxLifecycle
 import io.reactivex.Observable
 import io.reactivex.subjects.BehaviorSubject
+
+fun View.lifecycleProviderFromView(): LifecycleProvider<ViewEvent> {
+    val subject = BehaviorSubject.create<ViewEvent>()
+
+    addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+        override fun onViewAttachedToWindow(view: View?) {
+            subject.onNext(ViewEvent.ATTACH)
+        }
+
+        override fun onViewDetachedFromWindow(view: View) {
+            subject.onNext(ViewEvent.DETACH)
+        }
+    })
+
+    return object : LifecycleProvider<ViewEvent> {
+        override fun lifecycle(): Observable<ViewEvent> {
+            return subject.hide()
+        }
+
+        override fun <T : Any?> bindUntilEvent(event: ViewEvent): LifecycleTransformer<T> {
+            return RxLifecycle.bindUntilEvent<T, ViewEvent>(lifecycle(), event)
+        }
+
+        override fun <T : Any?> bindToLifecycle(): LifecycleTransformer<T> {
+            return bindUntilEvent(ViewEvent.DETACH)
+        }
+    }
+}
 
 abstract class RxView : LinearLayout, LifecycleProvider<ViewEvent> {
 
