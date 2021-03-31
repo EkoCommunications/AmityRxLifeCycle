a2d7337e5f86550e82eafee68c498694dbe50c5f
Pan <theptai@ekoapp.com>
2021-03-31 16:04:44 +0700

    Revert "nullable cast operator"
    This reverts commit 76ece16238ac0ab137030aa30eaa0946e9c90476.

--- rxlifecycle/src/main/java/com/ekoapp/rxlifecycle/extension/Maybe.kt
+++ rxlifecycle/src/main/java/com/ekoapp/rxlifecycle/extension/Maybe.kt
@@ -1,6 +1,8 @@
 package com.ekoapp.rxlifecycle.extension
 
+import android.app.Activity
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.trello.rxlifecycle3.LifecycleProvider
 import com.trello.rxlifecycle3.android.ActivityEvent
 import com.trello.rxlifecycle3.android.FragmentEvent
@@ -12,20 +14,27 @@ import io.reactivex.disposables.Disposable
 private val disposables = mutableMapOf<String, Disposable>()
 
 fun <E, T> Maybe<T>.untilLifecycleEnd(lifecycleProvider: LifecycleProvider<E>, uniqueId: String? = null): Maybe<T> {
-    return ((lifecycleProvider as? LifecycleProvider<ActivityEvent>)?.let {
-        bindUntilEvent(it, ActivityEvent.DESTROY)
-    } ?: (lifecycleProvider as? LifecycleProvider<FragmentEvent>)?.let {
-        bindUntilEvent(it, FragmentEvent.DESTROY)
-    } ?: (lifecycleProvider as? LifecycleProvider<ViewEvent>)?.let {
-        bindUntilEvent(it, ViewEvent.DETACH)
-    } ?: this)
-        .doOnSubscribe {
-            manageDisposables(it, uniqueId)
-        }.doOnDispose {
-            removeDisposable(uniqueId)
-        }.doOnTerminate {
-            removeDisposable(uniqueId)
-        }
+    return when (lifecycleProvider) {
+        is Activity -> bindUntilEvent(
+            lifecycleProvider as LifecycleProvider<ActivityEvent>,
+            ActivityEvent.DESTROY
+        )
+        is Fragment -> bindUntilEvent(
+            lifecycleProvider as LifecycleProvider<FragmentEvent>,
+            FragmentEvent.DESTROY
+        )
+        is View -> bindUntilEvent(
+            lifecycleProvider as LifecycleProvider<ViewEvent>,
+            ViewEvent.DETACH
+        )
+        else -> this
+    }.doOnSubscribe {
+        manageDisposables(it, uniqueId)
+    }.doOnDispose {
+        removeDisposable(uniqueId)
+    }.doOnTerminate {
+        removeDisposable(uniqueId)
+    }
 }
 
 fun <T> Maybe<T>.untilLifecycleEnd(view: View, uniqueId: String? = null): Maybe<T> {
